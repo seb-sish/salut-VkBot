34cfd5528ccb2af31efe628011d73dcd70f12d20
Sebsish <42474521+seb-sish@users.noreply.github.com>
2024-05-21 17:26:48 +0300

    disable parse links

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -14,7 +14,7 @@ start_text = "начать"
 async def start_handler(message: Message):
     ctx_storage.set("last", start_text)
     new_kb = await insert_buttons(kb_main(), Config.INFO.keys())
-    await message.answer("Выберите раздел", keyboard=new_kb.get_json())
+    await message.answer("Выберите раздел", keyboard=new_kb.get_json(), dont_parse_links=True)
 
 @main_labeler.message(text=["назад"])
 async def back_handler(message: Message):
@@ -24,14 +24,14 @@ async def back_handler(message: Message):
         to = json_search(Config.INFO, info[0][-2] if len(info[0]) >= 2 else info[0][-1])[1]
         ctx_storage.set("last", info[0][-2] if len(info[0]) >= 2 else start_text)
         new_kb = await insert_buttons(kb_main(), to["buttons"].keys())
-        await message.answer(to["text"], keyboard=new_kb.get_json(), attachment=to.get("image", ""))
+        await message.answer(to["text"], keyboard=new_kb.get_json(), attachment=to.get("image", ""), dont_parse_links=True)
     else: await error_handler(message)
 
 @main_labeler.message(text=Config.INFO.keys())
 async def main_handler(message: Message):
     ctx_storage.set("last", message.text)
     new_kb = await insert_buttons(kb_main(), Config.INFO[message.text]["buttons"].keys())
-    await message.answer(Config.INFO[message.text]["text"], keyboard=new_kb.get_json())
+    await message.answer(Config.INFO[message.text]["text"], keyboard=new_kb.get_json(), dont_parse_links=True)
     
 @main_labeler.message(text=Config.ADDITIONAL_BUTTONS)
 async def additional_handler(message: Message):
@@ -40,7 +40,7 @@ async def additional_handler(message: Message):
         data = info[1]
         ctx_storage.set("last", message.text)
         new_kb = await insert_buttons(kb_main(), data["buttons"].keys())
-        await message.answer(data["text"], keyboard=new_kb.get_json(), attachment=data.get("image", ""))
+        await message.answer(data["text"], keyboard=new_kb.get_json(), attachment=data.get("image", ""), dont_parse_links=True)
     else: await error_handler(message)
 
 @main_labeler.message()
